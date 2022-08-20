79b623cac5f11e928169876f17fa7051c3d34f6b
James Dickson <jamesdickson@Jamess-Air.home>
2022-08-20 16:04:28 -0500

    simple OAuth test

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const axios = require('axios');
+const open = require('open');
+
+const app = express()
+const port = process.env.PORT || 8080
+
+const CLIENT_ID = "apu9ebj084c1nxbhmuy";
+const CLIENT_SECRET = "9kzr66gepl0is1za29p";
+const tokenURL= 'https://api.smartsheet.com/2.0/token'
+const codeURL = 'https://app.smartsheet.com/b/authorize?response_type=code&client_id=' + CLIENT_ID + '&scope=READ_SHEETS%20WRITE_SHEETS'
+
+open(codeURL)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/authorize", (req, res) => {
+    axios({
+        method: "POST",
+        url: `${tokenURL}?grant_type=authorization_code&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`,
+        headers: {
+            Accept: "*/*"
+        },
+    }).then((response) => {
+        res.redirect(
+        `http://localhost:8080?access_token=${response.data.access_token}`
+        );
+        console.log(response.data.access_token)
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Your access code is: ${req.query.access_token}</h1>`)
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port '+ port)
+})
